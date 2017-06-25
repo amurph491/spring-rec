eecc6582ab1339652504305ec852391a2838721b
Alex Murphy <amurph491@gmail.com>
2017-06-25 18:38:51 -0400

    first commit

---
+++ src/main/kotlin/com/amurph491/videorec/VideorecApplication.kt
@@ -0,0 +1,11 @@
+package com.amurph491.videorec
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class VideorecApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(VideorecApplication::class.java, *args)
+}
---
+++ src/test/kotlin/com/amurph491/videorec/VideorecApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.amurph491.videorec
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class VideorecApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
